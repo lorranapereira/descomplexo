f2cb9fdab5b651b7339415cd437bf4b264d8e22b
lorranapereira <lorrana_pereira123@hotmail.com>
2017-11-12 20:01:45 -0200

    alteracao no circulo.js

--- www/js/circulo.js
+++ www/js/circulo.js
@@ -1,8 +1,9 @@
 const form = document.getElementById('form');
 const contem = document.getElementById('contem');
-console.log("entrou");
+let img = "undefined";
 class Raio {
   constructor(){
+    limpa ();
     console.log("entrou");
     let label = document.createElement("label");
     label.textContent = "Dígite o valor do raio";
@@ -11,24 +12,36 @@ class Raio {
     input.type = "number";
     input.id="raio";
     let button = document.createElement("BUTTON");
-    var img = document.createElement("IMG");
-    img.src =
-    "http://conversor-de-medidas.com/geometria/circulo/area/formula-da-area-do-circulo.png";
+    if (img !="undefined")  contem.removeChild(img);
+    img = document.createElement("IMG");
+    img.src ="circulo.png";
     button.type = "submit";
     button.style.width = "100px";
     button.textContent = "Calcular";
+    form.appendChild(contem);
     form.appendChild(label);
     form.appendChild(br);
     form.appendChild(input);
     form.appendChild(button);
+    form.appendChild(contem);
     contem.appendChild(img);
     
   }
 }
 
+function limpa () { 
+  let source = document.querySelector("#source").innerHTML;
+  let template = Handlebars.compile(source);
+  let html = template({
+    circulo: {coluna1:"", formula1:"", desenvolvimento1:"", resposta1:"", coluna2:"", formula2: "", desenvolvimento2:"", resposta2:"",coluna3:"", formula3:"", desenvolvimento3:"", resposta3:"",coluna4:"", formula4:"", desenvolvimento4:"", resposta4:"", coluna5:"", formula5: "", desenvolvimento5:"", resposta5:""}
+  });
+  document.querySelector('thead').innerHTML = html;
+  document.getElementById("form").innerHTML = "";
+  
+}
 class Cilindro {
   constructor(){
-   // form.reset();
+    limpa();
     let labelR = document.createElement("label");
     labelR.textContent = "Dígite o valor da raio";
     let br1 = document.createElement("BR");
@@ -44,9 +57,9 @@ class Cilindro {
     inputA.type = "number";
     inputA.id = "r";
     let button = document.createElement("BUTTON");
-    var img = document.createElement("IMG");
-    img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Cylinder_geometry.svg/518px-Cylinder_geometry.svg.png";
+    if (img != "undefined") contem.removeChild(img);   
+    img = document.createElement("IMG");
+    img.src = "cilindro.png"
     img.style.width = "100px";
     button.type = "submit";
     button.style.width = "100px";
@@ -60,6 +73,7 @@ class Cilindro {
     form.appendChild(inputA);
     form.appendChild(br4);
     form.appendChild(button);
+    form.appendChild(contem);
     contem.appendChild(img);
   }
 }
@@ -80,46 +94,48 @@ form.addEventListener('submit', function(e) {
   let valor = document.getElementById('selectOption').value;
   console.log(valor);
   if (valor == "circulo") {
-    console.log("kjojoj");
     let raio = document.getElementById("raio").value;
     raio = parseInt(raio);
     let source = document.querySelector("#source").innerHTML;
     let template = Handlebars.compile(source);
     let formulaArea = "A = 2 * π²";
-    let desenvolArea = "3,14 *"+raio;
-    let respostaArea = "A = "+3.14 * raio; 
-    let formulaPerimetro = "A = C²/4π";
-    let desenvolPerimetro = raio+" = C²/4π --> C² = "+raio+"/4π -->  C = √"+raio+"/4π";
-    let respostaPerimetro = "C = "+Math.sqrt(raio /(4 * 3,14));
+    let desenvolArea = "A = 3,14 *"+raio+"²";
+    let respostaArea = "A = "+3.14 * raio * raio; 
+    let formulaPerimetro = "P = 2πr";
+    let desenvolPerimetro = "P = 2 * 3.14 * "+raio;
+    let respostaPerimetro = "P = "+2*3.14*raio;
     let html = template({
-      circulo: {coluna1:"Área", formula1:formulaArea, desenvolvimeto1:desenvolArea, resposta1:respostaArea, coluna2:"Perímetro", formula2: formulaPerimetro, desenvolvimeto2:desenvolPerimetro, resposta2:respostaPerimetro}
+      circulo: {coluna1:"Área", formula1:formulaArea, desenvolvimento1:desenvolArea, resposta1:respostaArea, coluna2:"Perímetro", formula2: formulaPerimetro, desenvolvimento2:desenvolPerimetro, resposta2:respostaPerimetro}
     });
     document.querySelector('thead').innerHTML = html;
   }
   if (valor == "cilindro") {
     let raio = document.getElementById("r").value;
+    raio = parseInt(raio);
     let altura = document.getElementById("h").value;
-    let source = document.querySelector("#source").innerHTML;
-    let template = Handlebars.compile(source);
+    altura = parseInt(altura);
     let abase = 3.14 * raio * raio;
     let alateral = 2 * 3.14 * altura * raio;
+    let total = 2 * abase - alateral
     let formulaBase = "Abase = π * r²";
     let formulaLateral = "Alateral = 2 * π * r * h";
     let formulaVolume = "V = h * π *r²";
     let formulaTotal = "Atotal = 2 * Abase - Alateral";
-    let formulaPerimetro = "P = 2π * h";
-    let desenvolBase = "3,14 * "+raio+"²";
-    let desenvolLateral = "Atotal = (2 * 3,14 "+raio+") - "+altura;
+    let formulaPer = "P = 2π * h";
+    let desenvolBase = "A = 3,14 * "+raio+"²";
+    let desenvolLateral = "Alateral = (2 * 3,14 "+raio+") * "+altura;
     let desenvolVolume = "V = "+altura+" * 3,14 *"+raio * raio;
     let desenvolTotal = "Atotal = 2 * "+abase+" - "+alateral;
-    let desenvolPer = "2 * 3,14 * "+altura;
+    let desenvolPer = "P = 2 * 3,14 * "+altura;
     let respostaBase = "Abase = "+abase; 
     let respostaLateral = "Alateral = "+alateral;
-    let respostaVolume = "V = "+altura* 3.14 * raio * raio;
-    let respostaTotal = "Atotal = "+(2 * altura)-alateral ;
+    let respostaVolume = "V = "+ abase * altura;
+    let respostaTotal = "Atotal = "+total;
     let respostaPer = "P = "+2 * 3.14 * altura ;
-    var html = template({
-      circulo: {coluna1:"Área da base", formula1:formulaArea, desenvolvimeto1:desenvolArea, resposta1:respostaArea, coluna2:"Área lateral", formula2: formulaPerimetro, desenvolvimeto2:desenvolPerimetro, resposta2:respostaPerimetro,coluna3:"Volume", formula3:formulaVolume, desenvolvimeto1:desenvolVolume, resposta1:respostaVolume,coluna4:"Área total", formula4:formulaArea, desenvolvimeto4:desenvolArea, resposta4:respostaArea, coluna5:"Perímetro", formula5: formulaPerimetro, desenvolvimeto5:desenvolPer, resposta5:respostaPer}
+    let source = document.querySelector("#source").innerHTML;
+    let template = Handlebars.compile(source);
+    let html = template({
+      circulo: {coluna1:"Área da base", formula1:formulaBase, desenvolvimento1:desenvolBase, resposta1:respostaBase, coluna2:"Área lateral", formula2:formulaLateral, desenvolvimento2:desenvolLateral, resposta2:respostaLateral,coluna3:"Volume", formula3:formulaVolume, desenvolvimento3:desenvolVolume, resposta3:respostaVolume,coluna4:"Área total", formula4:formulaTotal, desenvolvimento4:desenvolTotal, resposta4:respostaTotal, coluna5:"Perímetro", formula5: formulaPer, desenvolvimento5:desenvolPer, resposta5:respostaPer}
     });
     document.querySelector('thead').innerHTML = html;
   }
