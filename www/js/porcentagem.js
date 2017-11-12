26ee0f04582aa8e729c8931a2b937972dfd742d9
lorranapereira <lorrana_pereira123@hotmail.com>
2017-11-12 11:45:29 -0200

    Update porcentagem.js

--- www/js/porcentagem.js
+++ www/js/porcentagem.js
@@ -5,18 +5,24 @@ const form = document.getElementById('form');
 
 
 form.addEventListener('submit', function(e) {
-console.log("iiii");    
     let porcent = document.getElementById("porcent").value;
     let de =  document.getElementById("de").value;
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
-    p1.textContent = porcent +"------"+de;
+    let p4 = document.createElement("p");
+    let p5 = document.createElement("p");
+
+    p1.textContent = porcent +"% - - - - "+de;
+    p2.textContent = "100% - - - - x";
+    p3.textContent = porcent +"x = "+ 100 * de;
+    p4.textContent = "x = "+ 100 * de+"/"+porcent;
+    p5.textContent = "x = "+ (100 * de)/porcent; 
     contem.appendChild(p1); 
-    p2.textContent = "100 ------ 1";
     contem.appendChild(p2);
-    p3.textContent = porcent * de +"/"+100+"="+ (porcent * de) / 100;
     contem.appendChild(p3);
+    contem.appendChild(p4);
+    contem.appendChild(p5);
     e.preventDefault();
-}); 
+});
 
