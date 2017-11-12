8cf829ff0b207cd83769f33d7c1ce28cc4d19428
lorranapereira <lorrana_pereira123@hotmail.com>
2017-11-12 11:43:38 -0200

    Update jurosC.js

--- www/js/jurosC.js
+++ www/js/jurosC.js
@@ -31,25 +31,6 @@ form.addEventListener('submit', function(e) {
         contem.appendChild(p4);
         contem.appendChild(p5);
     }
-/* envolve o logaritmo e o i falta  if (i == "x") {
-        m = Number(document.getElementById("m").value);
-        j = Number(document.getElementById("j").value);
-        i = document.getElementById("i").value;
-        t = Number(document.getElementById("t").value);
-        c = Number(document.getElementById("c").value);
-        p1 = document.createElement("p");
-        p2 = document.createElement("p");
-        p3 = document.createElement("p");
-        p4 = document.createElement("p");
-        p1.textContent = "J = C * i * T";
-        p2.textContent = j+"="+ c*t+"i/100";
-        p3.textContent = c*t+"i = "+j+" * 100";
-        p4.textContent = "i = "+((j*100)/(c*t)) +"% = "+((j*100)/(c*t))/100;
-        contem.appendChild(p1);
-        contem.appendChild(p2);
-        contem.appendChild(p3);
-        contem.appendChild(p4);
-    }*/
     if (c == "x") {
         console.log("plpl");
         m = Number(document.getElementById("m").value);
