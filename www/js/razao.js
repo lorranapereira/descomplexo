8bb53cab46493a0993a1550279af896081505e44
lorranapereira <lorrana_pereira123@hotmail.com>
2017-11-12 11:47:35 -0200

    Update razao.js

--- www/js/razao.js
+++ www/js/razao.js
@@ -6,43 +6,53 @@ form.addEventListener('submit', function(e) {
     valorx = parseInt(document.getElementById('x').value);
     valory = parseInt(document.getElementById('y').value);
     console.log("oii");
-    razao(valorx,valory);
+    let razao = new Razao(valorx,valory) ;
     e.preventDefault();
 });
-
-function razao(valorx,valory) {
-    console.log("oii");
-    let html="";
-    this.x = valorx;
-    this.y = valory;
-    let maior = Math.max(this.x,this.y); 
-    let menor = Math.min(this.x,this.y); 
-    if (maior%menor == 0) {
-        if(this.x>this.y){
-            html += this.x+"/"+this.y+" = "+maior/menor+"/"+menor/menor;
-        }
-        else{
-            html += this.x+"/"+this.y+" = "+menor/menor+"/"+maior/menor;
-        }
-            let p = document.createElement('P');
-            p.textContent = html;
-            contem.appendChild(p);
-    } 
-    else{
-        for (let i = 0; i <= maior; i++) {
-            if(maior % i == 0 && menor % i == 0 && i!=maior){
-                html += this.x/i+"/"+this.y/i+" + ";   
+class Razao {
+    constructor(valorx,valory) {
+        this.x = valorx;
+        this.y = valory;
+        this.maior = Math.max(this.x,this.y); 
+        this.menor = Math.min(this.x,this.y); 
+        let html="";
+        if (this.maior%this.menor == 0) {
+            if(this.x>this.y){
+                html += this.x+"/"+this.y+" = "+this.maior/this.menor+"/"+this.menor/this.menor;
             }
-            if(i == maior){
-                let last = html.lastIndexOf('+');
-                html = html.slice(0,last-1);
-                let antes = html.slice(0,html.lastIndexOf('+')-1);
-                let depois = html.slice(html.lastIndexOf('+')+1, html.length);
-                let html2 = antes+" = "+depois;
+            else{
+                html += this.x+"/"+this.y+" = "+this.menor/this.menor+"/"+this.maior/this.menor;
+            }
                 let p = document.createElement('P');
-                p.textContent = html2;
+                p.textContent = html;
                 contem.appendChild(p);
-                p = "";
+        } 
+        else{
+            console.log("2");
+            for (let i = 0; i <= this.maior; i++) {
+                if(this.maior % i == 0 && this.menor % i == 0 && i!=this.maior){
+                    console.log(this.x);
+                    html += this.x/i+"/"+this.y/i+" + ";   
+                }
+                if (this.x.toString().length == 1 && this.maior%this.menor!=0) {
+                   console.log("entou");
+                   let p = document.createElement('P');
+                   p.textContent = "x = "+this.x+"/"+this.y; 
+                   return contem.appendChild(p); 
+                }
+                if(i == this.maior){
+                    console.log("eeeh");
+                    let last = html.lastIndexOf('+');
+                    html = html.slice(0,last-1);
+                    let antes = html.slice(0,html.lastIndexOf('+')-1);
+                    let depois = html.slice(html.lastIndexOf('+')+1, html.length);
+                    let html2 = antes+" = "+depois;
+                    let p = document.createElement('P');
+                    let p1 = document.createElement('P');
+                    
+                    p.textContent = "x = "+html2;
+                    contem.appendChild(p);
+                }
             }
         }
     }
